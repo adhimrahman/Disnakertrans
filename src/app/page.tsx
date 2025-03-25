f8bd0bb6ac0a1adf21857d00db068ccf666685cf
adhimrahman <adhimrahaman@gmail.com>
2025-03-25 23:42:00 +0800

    feat: test layouting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,17 @@ export default function Home() {
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
 				<h1>CH3</h1>
+				<div className="sideBar">
+
+				</div>
+				<div className="main">
+					<div className="topSide">
+
+					</div>
+					<div className="bottomSide">
+						
+					</div>
+				</div>
 			</main>
 		</div>
 	);
