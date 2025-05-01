b264c6cd1ead7166ad1887e05ac0e01826de311c
gerynsb <118332285+gerynsb@users.noreply.github.com>
2025-05-02 02:08:54 +0800

    Fix : Sidebar

--- src/components/dashboard/Card.tsx
+++ src/components/dashboard/Card.tsx
@@ -1,20 +1,33 @@
+
+import React, { ReactNode } from 'react';
+
 type CardProps = {
   className?: string; // untuk control size
   title: string;
   body: string;
   icon: React.ReactNode;
   onClick?: () => void;
+  titleClassName?: string;
+  bodyClassName?: string;
+  iconWrapperClassName?: string;
 };
 
-export default function Card({className = '', title, body, icon, onClick }: CardProps) {
+
+export default function Card({className = '', title, body, icon, onClick, 
+  titleClassName = "",
+  bodyClassName = "",
+  iconWrapperClassName = ""
+}: CardProps) {
+  
+
   return (
     <div
       className={`cursor-pointer bg-white rounded-xl shadow-2xl overflow-hidden ${className}`}
     >
       <div className="flex flex-col justify-center items-center text-center gap-3 p-4" onClick={onClick}>
-        <div className="text-8xl text-blue-500">{icon}</div> {/* ✅ Tampilkan icon */}
-        <h2 className="text-lg font-base text-black">{title}</h2>
-        <p className="text-sm text-gray-700">{body}</p>
+        <div className="text-8xl mb-8 text-blue-500 mt-10">{icon}</div> {/* ✅ Tampilkan icon */}
+        <h2 className="text-lg font-base mb-4 text-black">{title}</h2>
+        <p className="text-sm text-gray-700 max-w-xs px-4">{body}</p>
       </div>
     </div>
   );
