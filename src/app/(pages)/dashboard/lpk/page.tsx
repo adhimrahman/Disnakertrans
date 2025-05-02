6b8f4756cfaee33df72637a899ad9ce38e20f9dc
gerynsb <118332285+gerynsb@users.noreply.github.com>
2025-05-02 18:19:06 +0800

    Add : dashboard lpk

--- src/app/(pages)/dashboard/lpk/page.tsx
+++ src/app/(pages)/dashboard/lpk/page.tsx
@@ -1,12 +1,17 @@
-import ProtectedRoute from "@/components/ProtectedRoute";
+// dashboard/lpk/page.tsx
+import React from 'react';
+import DashboardLayout from './layout'; // Pastikan path ini sesuai dengan strukt
 
-export default function LPKDashboard() {
-	return (
-		<ProtectedRoute>
-			<div>
-				<h1>Dashboard LPK</h1>
-				<p>Selamat datang di Dashboard LPK!</p>
-			</div>
-		</ProtectedRoute>
-	);
-}
+const LPKPage: React.FC = () => {
+  return (
+    <DashboardLayout>
+      <div className="content">
+        <h2 className="text-2xl font-semibold">Selamat datang di Dashboard LPK!</h2>
+        {/* Konten spesifik untuk halaman LPK */}
+        <p>Ini adalah halaman utama untuk LPK. Anda bisa menambahkan konten lainnya di sini.</p>
+      </div>
+    </DashboardLayout>
+  );
+};
+
+export default LPKPage;
