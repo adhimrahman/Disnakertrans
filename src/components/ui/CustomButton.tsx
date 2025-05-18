52daa519f8573b964ecb3edd418932666397d800
adhimrahman <adhimrahaman@gmail.com>
2025-05-18 17:11:47 +0800

    feat: add variant prop and updated button styles

--- src/components/ui/CustomButton.tsx
+++ src/components/ui/CustomButton.tsx
@@ -7,16 +7,27 @@ type CustomButtonProps = {
 	className?: string;
 	px?: number;
 	py?: number;
+	variant?: "red" | "blue";
 };
 
 const CustomButton: React.FC<CustomButtonProps> = (
-	{ text, width = 'w-auto', height = 'h-auto', px = 'px-0', py = 'py-0', className = '' }
+	{ text, width = 'w-auto', height = 'h-auto', px = 'px-0', py = 'py-0', className = '', variant = 'red' }
 ) => {
+	const colorClasses =
+		variant === "blue"
+			? "bg-blue-500 hover:bg-blue-700"
+			: "bg-red-500 hover:bg-red-700";
+	
 	return (
 		<button
-			className={`bg-red-500 text-white rounded-xl shadow-lg
-			hover:bg-red-700 transition hover:cursor-pointer capitalize 
-			px-${px} py-${py} ${width} ${height} ${className}`}
+			className={`
+				text-white rounded-xl shadow-lg font-semibold
+				transition hover:cursor-pointer capitalize 
+				px-${px} py-${py}
+				${width} ${height}
+				${className}
+				${colorClasses}
+			`}
 		>
 			{text}	
 		</button>
