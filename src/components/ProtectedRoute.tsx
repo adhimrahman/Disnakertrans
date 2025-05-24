3074f20a2c10f3fbe167c8371ad014ce6ae272a2
adhimrahman <adhimrahaman@gmail.com>
2025-05-24 18:14:00 +0800

    feat: add spinne loader compo

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -6,6 +6,7 @@ import { auth, db } from "@/firebase/config";
 import { doc, getDoc } from "firebase/firestore";
 import Modal from "react-modal";
 import { handleLogout } from "@/lib/auth";
+import LoadingSpinner from "@/components/ui/LoadingSpinner";
 
 type ProtectedRouteProps = {
     children: React.ReactNode;
@@ -16,7 +17,6 @@ type ProtectedRouteProps = {
 const ProtectedRoute = ({ children, expectedRole, checkLpkId = false }: ProtectedRouteProps) => {
     const router = useRouter();
     const params = useParams();
-    // const [isAuthenticated, setIsAuthenticated] = useState(false);
     const [isAuthorized, setIsAuthorized] = useState<boolean | null>(null);
     const [isModalOpen, setIsModalOpen] = useState(false);
 
@@ -74,7 +74,7 @@ const ProtectedRoute = ({ children, expectedRole, checkLpkId = false }: Protecte
     }
 
     if (isAuthorized === null) {
-        return <div className="text-white">Loading...</div>;
+        return <LoadingSpinner />;
     }
 
     if (isAuthorized === false) {
@@ -100,92 +100,6 @@ const ProtectedRoute = ({ children, expectedRole, checkLpkId = false }: Protecte
         );
     }
     return <>{children}</>;
-=======
-
-type ProtectedRouteProps = {
-	children: React.ReactNode;
-	expectedRole: "disnaker" | "lpk";
-	checkLpkId?: boolean;
-};
-
-const ProtectedRoute = ({ children, expectedRole, checkLpkId = false }: ProtectedRouteProps) => {
-	const router = useRouter();
-	const params = useParams();
-	// const [isAuthenticated, setIsAuthenticated] = useState(false);
-	const [isAuthorized, setIsAuthorized] = useState<boolean | null>(null);
-	const [isModalOpen, setIsModalOpen] = useState(false);
-
-	useEffect(() => {
-		const unsubscribe = onAuthStateChanged(auth, async (user) => {
-			if (!user) {
-				setIsAuthorized(false);
-				setIsModalOpen(true);
-				return;
-			}
-
-			const docRef = doc(db, "akun", user.uid);
-			const docSnap = await getDoc(docRef);
-
-			if (!docSnap.exists()) {
-				setIsAuthorized(false);
-				setIsModalOpen(true);
-				return;
-			}
-
-			const userData = docSnap.data();
-			const role = userData.role;
-
-			if (role !== expectedRole) {
-				setIsAuthorized(false);
-				setIsModalOpen(true);
-				return;
-			}
-
-			if (checkLpkId) {
-				const urlLpkId = params?.lpkId;
-				const userLpkId = userData.lpkId;
-
-				if (urlLpkId !== userLpkId) {
-					setIsAuthorized(false);
-					setIsModalOpen(true);
-					return;
-				}
-			}
-
-			setIsAuthorized(true);
-		});
-
-		return () => unsubscribe();
-	}, [expectedRole, checkLpkId, params]);
-
-	const handleRedirect = () => {
-		setIsModalOpen(false);
-		router.push("/login");
-	};
-
-	if (isAuthorized === null) {
-		return <div className="text-white">Loading...</div>;
-	}
-
-	if (isAuthorized) {
-		return <>{children}</>;
-	}
-
-	return (
-		<Modal isOpen={isModalOpen} onRequestClose={() => setIsModalOpen(false)} contentLabel="Login Required" ariaHideApp={false}
-			className="fixed inset-0 bg-gray-900 bg-opacity-50 flex items-center justify-center z-50"
-		>
-			<div className="bg-white p-6 rounded-xl max-w-lg mx-auto">
-				<h2 className="text-2xl font-semibold mb-4 text-black">Anda perlu login terlebih dahulu</h2>
-				<p className="text-gray-700 mb-4">
-					Untuk mengakses halaman ini, Anda harus masuk terlebih dahulu.
-				</p>
-				<button onClick={handleRedirect} className="bg-red-500 text-white px-6 py-2 rounded-lg mt-4 cursor-pointer">
-					Ke Halaman Login
-				</button>
-			</div>
-		</Modal>
-	);
 };
 
 export default ProtectedRoute;
