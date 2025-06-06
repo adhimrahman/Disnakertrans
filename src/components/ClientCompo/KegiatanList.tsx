a80261e5d86c7d39672da45a3ee5169684d0d94c
adhimrahman <adhimrahaman@gmail.com>
2025-06-06 09:50:35 +0800

    feat: add server and client component for kegiatan

--- src/components/ClientCompo/KegiatanList.tsx
+++ src/components/ClientCompo/KegiatanList.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
+import CustomButton from "@/components/ui/CustomButton";
 
 type KegiatanItem = {
     id: string;
@@ -11,47 +12,23 @@ type KegiatanItem = {
     Tanggal?: string;
 };
 
-function limitChars(text: string, maxLength: number) {
-	return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
-}
-
 export default function KegiatanList({ kegiatan } : { kegiatan: KegiatanItem[] }) {
     const [visibleCount, setVisibleCount] = useState(9);
     const visibleKegiatan = kegiatan.slice(0, visibleCount);
-    const isLoading = kegiatan.length === 0;
-
-    const handleLoadMore = () => {
-		setVisibleCount(kegiatan.length);
-	};
+    const handleLoadMore = () => { setVisibleCount(kegiatan.length) };
 
     return (
         <section className="py-16 lg:py-20 px-6 md:px-24 bg-gradient-to-b from-white to-gray-100">
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                {isLoading ? (
-                    Array.from({ length: 9 }).map((_, index) => (
-                        <div key={index} className="relative rounded-lg overflow-hidden border border-gray-200 shadow-md animate-pulse bg-white">
-                            <div className="w-full h-60 bg-gray-300" />
-                            <div className="py-13 px-6">
-                                <div className="h-5 bg-gray-300 rounded w-3/4 mb-2" />
-                                <div className="h-4 bg-gray-200 rounded w-full mb-2" />
-                                <div className="h-4 bg-gray-200 rounded w-5/6" />
-                            </div>
-                        </div>
-                    ))
-                ) : (
-                    visibleKegiatan.map((item) => (
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-x-6 gap-y-9">
+                    {visibleKegiatan.map((item) => (
                         <div key={item.id} className="relative group rounded-lg overflow-hidden border border-gray-200 shadow-md hover:shadow-xl transition-all duration-300 transform hover:scale-105">
                             <div className="w-full h-60 relative overflow-hidden">
-                                {item.ImageSampul ? ( 
-                                    <Image src={item.ImageSampul} alt={item.Judul} layout="fill" className="object-cover object-center" />
-                                ) : (
-                                    <div className="w-full h-full bg-gray-200" />
-                                )}
+                                <Image src={item.ImageSampul} alt={item.Judul} layout="fill" className="object-cover object-center" sizes="100vw" />
                             </div>
         
                             <div className="p-6 bg-white">
-                                {/* <h3 className="text-xl font-semibold text-gray-800 mb-2 capitalize">{item.Judul}</h3> */}
-                                <h3 className="text-xl font-semibold text-gray-800 mb-2 capitalize">{limitChars(item.Judul, 26)}</h3>
+                                <h3 className="text-xl font-semibold text-gray-800 mb-2 capitalize hover:cursor-pointer line-clamp-1"
+                                title={item.Judul}>{item.Judul}</h3>
                                 <p className="text-sm text-gray-600 line-clamp-3">{item.Deskripsi}</p>
                                 <Link className="mt-3 inline-block text-sm text-blue-600 hover:text-blue-800 font-medium"
                                 href={`/kegiatan/${item.id}`}>
@@ -59,16 +36,12 @@ export default function KegiatanList({ kegiatan } : { kegiatan: KegiatanItem[] }
                                 </Link>
                             </div>
                         </div>
-                    ))
-                )}
+                    ))}
             </div>
 
-            {/* Load More Button */}
             {visibleCount < kegiatan.length && (
-                <div className="flex justify-center mt-14">
-                    <button className="px-8 py-3 bg-blue-600 hover:bg-blue-700 text-white rounded-full font-semibold transition duration-300" onClick={handleLoadMore} >
-                        Tampilkan Semua
-                    </button>
+                <div className="flex justify-center mt-15">
+                    <CustomButton text="Tampilkan Semua" variant="blue" px={8} py={3} className="rounded-full" onClick={handleLoadMore}/>
                 </div>
             )}
         </section>
--- src/components/home/Kegiatan.tsx
+++ src/components/home/Kegiatan.tsx
@@ -1,80 +1,17 @@
-"use client"
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import Image from "next/image";
-import { ArrowRight } from "lucide-react";
-import Link from "next/link";
-import type { KegiatanItem } from "@/lib/getKegiatan";
+import KegiatanHome from "@/components/ClientCompo/KegiatanHome";
+import { KegiatanItem } from "@/lib/getKegiatan";
 
-function limitChars(text: string, maxLength: number) {
-	return text.length > maxLength ? text.slice(0, maxLength) + "..." : text;
-}
+type KegiatanProps = {
+  kegiatan: KegiatanItem[];
+};
 
-function limitWords(text: string, count: number) {
-	const words = text.split(" ");
-	return words.slice(0, count).join(" ") + (words.length > count ? "..." : "");
-}
-
-export default function KegiatanHome({ kegiatan }: { kegiatan: KegiatanItem[] }) {
-	const isLoading = !Array.isArray(kegiatan) || kegiatan.length === 0;
-
+export default async function KegiatanCarousel({ kegiatan }: KegiatanProps) {
 	return (
 		<section className="pt-16 pb-10 px-5 bg-gray-100">
 			<h2 className="text-4xl font-bold text-center mb-10 text-gray-800 capitalize">
 				Kegiatan Disnaker Gowa
 			</h2>
-			
-			<Swiper modules={[Navigation]} spaceBetween={-35} navigation breakpoints={{
-				0: { slidesPerView: 1 }, 640: { slidesPerView: 1.2 }, 768: { slidesPerView: 2 },
-				1024: { slidesPerView: 3 }, 1280: { slidesPerView: 4 },
-			}}>
-				{isLoading ? (
-					Array.from({ length: 4 }).map((_, index) => (
-						<SwiperSlide key={index}>
-							<div className="bg-white shadow-md rounded-2xl overflow-hidden animate-pulse m-4 w-full max-w-xs mx-auto">
-								<div className="w-full aspect-[4/3] bg-gray-300" />
-								<div className="p-4 md:p-5 flex flex-col gap-2">
-									<div className="h-5 bg-gray-300 rounded w-3/4" />
-									<div className="h-3 bg-gray-300 rounded w-full" />
-									<div className="h-3 bg-gray-300 rounded w-5/6" />
-									<div className="h-3 bg-gray-300 rounded w-2/3" />
-								</div>
-								<div className="px-4 md:px-5 py-4 flex justify-end">
-									<div className="h-8 w-24 bg-gray-300 rounded" />
-								</div>
-							</div>
-						</SwiperSlide>
-					))
-				) : (
-					kegiatan.map((item) => (
-						<SwiperSlide key={item.id}>
-							<div className="bg-white shadow-md rounded-2xl overflow-hidden hover:shadow-xl transition transform hover:-translate-y-2 m-4 flex flex-col justify-between h-full w-full max-w-xs mx-auto">
-								{item.ImageSampul ? (
-									<Image width={360} height={360} src={item.ImageSampul} alt={item.Judul} className="w-full aspect-[4/3] object-cover" loading="lazy" />
-								) : (
-									<div className="w-full aspect-[4/3] bg-gray-200" />
-								)}
-								<div className="p-4 md:p-5 text-justify flex flex-col gap-1">
-									<h3 className="text-lg md:text-xl font-bold text-gray-900 mb-1 fade-text-end">
-										{/* {item.Judul} */}
-										{limitChars(item.Judul, 26)}
-									</h3>
-									<p className="text-gray-700 text-sm leading-relaxed pt-2">
-										{limitWords(item.Deskripsi, 15)}
-									</p>
-								</div>
-								<div className="px-4 md:px-5 py-4 flex justify-end">
-									<Link className="bg-blue-700 text-white px-5 py-2 rounded-md shadow hover:bg-blue-800 transition text-sm md:text-base tracking-wider flex items-center gap-1"
-										href={`/kegiatan/${item.id}`}> Selengkapnya <ArrowRight size={16} />
-									</Link>
-								</div>
-							</div>
-						</SwiperSlide>
-					))
-				)}
-			</Swiper>
+			<KegiatanHome kegiatan={kegiatan} />
 		</section>
 	);
 }
