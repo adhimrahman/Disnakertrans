c38ff23277e76d531acda7aedeb09f559232e984
gerynsb <deangerypasamba@gmail.com>
2025-06-12 04:45:46 +0800

    edit sedikit

--- src/app/(pages)/dashboard/lpk/[lpkId]/pelatihan/page.tsx
+++ src/app/(pages)/dashboard/lpk/[lpkId]/pelatihan/page.tsx
@@ -34,6 +34,7 @@ export default async function PelatihanDashboardPage({ params, searchParams }: P
           { value: "Judul", label: "Judul" },
         ]}
       />
+
     </div>
   );
 }
--- src/models/Pelatihan.ts
+++ src/models/Pelatihan.ts
@@ -1,3 +1,14 @@
+export interface Pelatihan {
+  judul: string;
+  deskripsi: string;
+  gambar_pelatihan: string;
+  link_form: string;
+  tanggal_kegiatan?: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any; // Membolehkan akses key lain dengan tipe string
+}
+
+
 export interface PelatihanItem {
   id: string;
   judul: string;
