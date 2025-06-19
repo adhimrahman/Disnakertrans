5a77c62d77ba00eb51c489491c52c183dfa17437
gerynsb <pasambadg22d@student.unhas.ac.id>
2025-06-20 01:45:28 +0800

    fix : edit pelatihan

--- src/validation/pelatihan-validation.ts
+++ src/validation/pelatihan-validation.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const CreatePelatihanSchema = z.object({
-	judul: z.string().min(5, "Judul harus diisi dan jelas").max(30, "Judul maksimal 30 karakter"),
-	deskripsi: z.string().min(5, "Deskripsi harus diisi dan jelas").max(1000, "Deskripsi maksimal 1000 karakter"),
+	judul: z.string().min(5, "Judul harus diisi dan jelas").max(100, "Judul maksimal 100 karakter"),
+	deskripsi: z.string().min(5, "Deskripsi harus diisi dan jelas").max(10000, "Deskripsi maksimal 10000 karakter"),
 	gambar_pelatihan: z.string().url("Gambar harus berupa URL"),
 	link_form: z.string().url("Link harus berupa URL"),
 	tanggal_kegiatan: z.string().date("Tanggal Kegiatan harus diisi"),
@@ -12,8 +12,8 @@ export const GetPelatihanSchema = z.string().min(1, "ID Pelatihan tidak valid");
 
 export const UpdatePelatihanSchema = z.object({
 	id: z.string().min(1, "ID Pelatihan tidak valid"),
-	judul: z.string().min(5, "Judul harus diisi dan jelas").max(30, "Judul maksimal 30 karakter").optional(),
-	deskripsi: z.string().min(5, "Deskripsi harus diisi dan jelas").max(1000, "Deskripsi maksimal 1000 karakter").optional(),
+	judul: z.string().min(5, "Judul harus diisi dan jelas").max(100, "Judul maksimal 100 karakter").optional(),
+	deskripsi: z.string().min(5, "Deskripsi harus diisi dan jelas").max(10000, "Deskripsi maksimal 10000 karakter").optional(),
 	gambar_pelatihan: z.string().url("Gambar harus berupa URL").optional(),
 	link_form: z.string().url("Link harus berupa URL").optional(),
 	tanggal_kegiatan: z.string().date("Tanggal Kegiatan harus diisi").optional(),
