ca5969734bee5769df93a89bd50f5d7140360658
adhimrahman <adhimrahaman@gmail.com>
2025-06-20 04:29:21 +0800

    `Removed search functionality and simplified KegiatanDashboardPage component`

--- src/app/(dashboard)/disnaker/konten/kegiatan/page.tsx
+++ src/app/(dashboard)/disnaker/konten/kegiatan/page.tsx
@@ -1,37 +1,12 @@
 import KegiatanList from "@/components/dashboard/KegiatanList";
-import SearchSortControls from "@/components/dashboard/SearchandSort";
-import { getKegiatanFilteredByJudulContains, getKegiatanByDateAndSort } from "@/firebase/utils/kegiatan-service";
-import { KegiatanItem } from "@/models/Kegiatan";
+import { getKegiatanByDateAndSort } from "@/firebase/utils/kegiatan-service";
 
-interface KegiatanDashboardPageProps {
-    searchParams?: {
-        search?: string;
-        sort?: keyof KegiatanItem;
-        order?: "asc" | "desc";
-    };
-}
+export default async function KegiatanDashboardPage() {
+  const kegiatan = await getKegiatanByDateAndSort("created_at", "asc");
 
-export default async function KegiatanDashboardPage({ searchParams }: KegiatanDashboardPageProps) {
-    const {
-        search = '',
-        sort = 'created_at',
-        order = 'asc',
-    } = searchParams || {};
-
-    const kegiatan = search
-        ? await getKegiatanFilteredByJudulContains(search, sort, order) 
-        : await getKegiatanByDateAndSort(sort, order);
-
-    return (
-        <div className="flex flex-col gap-y-4 bg-white rounded-md p-4">
-            <SearchSortControls
-                sortOptions={[
-                    { value: "created_at", label: "Tanggal" },
-                    { value: "judul", label: "Judul" },
-                ]}
-            />
-            <span className="px-3" />
-            <KegiatanList kegiatan={kegiatan} />
-        </div>
-    );
+  return (
+    <div className="flex flex-col gap-y-4 bg-white rounded-md p-4">
+      <KegiatanList kegiatan={kegiatan} />
+    </div>
+  );
 }
